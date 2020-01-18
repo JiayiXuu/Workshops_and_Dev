a0613b3f8e291080aed0081dc6ca409298b9347e
JiayiXuu <jiayixu@ad.unc.edu>
2020-01-18 18:17:24 -0500

    Workshop_basketball

---
+++ workshop_basketball/Assets/script/GameBehavior.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameBehavior : MonoBehaviour
+{
+    public GameObject basketball;
+
+    private float speed = 10.0f;
+    private float rotationSpeed = 100.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(basketball, new Vector3(0, 4, 0), Quaternion.identity);
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float translation = Input.GetAxis("Vertical") * speed;
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+
+        // Make it move 10 meters per second instead of 10 meters per frame...
+        translation *= Time.deltaTime;
+        rotation *= Time.deltaTime;
+
+        // Move translation along the object's z-axis
+        transform.Translate(0, 0, translation);
+
+        // Rotate around our y-axis
+        transform.Rotate(0, rotation, 0);
+    }
+}
